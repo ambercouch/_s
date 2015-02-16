3d666c96eb7d6126ba94366cd389fc21c030fad9
Richiearnold <richard@ambercouch.co.uk>
2015-02-16 10:52:39 +0000

    added type kit

--- header.php
+++ header.php
@@ -9,6 +9,32 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
   <head>
+    <script>
+      (function (d) {
+        var config = {
+          kitId: 'aaq4chq',
+          scriptTimeout: 3000
+        },
+        h = d.documentElement, t = setTimeout(function () {
+          h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
+        }, config.scriptTimeout), tk = d.createElement("script"), f = false, s = d.getElementsByTagName("script")[0], a;
+        h.className += " wf-loading";
+        tk.src = '//use.typekit.net/' + config.kitId + '.js';
+        tk.async = true;
+        tk.onload = tk.onreadystatechange = function () {
+          a = this.readyState;
+          if (f || a && a != "complete" && a != "loaded")
+            return;
+          f = true;
+          clearTimeout(t);
+          try {
+            Typekit.load(config)
+          } catch (e) {
+          }
+        };
+        s.parentNode.insertBefore(tk, s)
+      })(document);
+    </script>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
