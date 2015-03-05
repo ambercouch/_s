91fdd2a5f51f456c966373f8a572eb4dc8913002
Richiearnold <richard@ambercouch.co.uk>
2015-03-05 22:13:26 +0000

    updated site layouts

--- header.php
+++ header.php
@@ -21,14 +21,16 @@
     <div id="page" class="hfeed site">
       <a class="skip-link screen-reader-text" href="#content"><?php _e('Skip to content', '_s'); ?></a>
 
-      <header id="masthead" class="site__header" role="banner">
-        <div class="site__branding">
-          <div class="branding">
-            <h1 class="branding__title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-            <h2 class="branding__description"><?php bloginfo('description'); ?></h2>
-          </div><!-- .branding -->
-        </div><!-- .site-branding -->
-      </header><!-- #masthead -->
+      <header id="masthead" class="site__master-header" role="banner">
+        <div class="master-header">
+          <div class="master-header__branding">
+            <div class="branding">
+              <h1 class="branding__title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+              <h2 class="branding__description"><?php bloginfo('description'); ?></h2>
+            </div><!-- .branding -->
+          </div><!-- .master-header__branding -->
+        </div><!-- .master-header -->
+      </header><!-- .site__master-header -->
       <nav id="main-navigation" class="site__navigation" role="navigation">
         <div class="navigation--main">
           <button class="navigation--main__toggle" aria-controls="navigation--main__menu" aria-expanded="false"><?php _e('Primary Menu', '_s'); ?></button>
